73c42933f385bf48bca4e52c4ab323cace0bd807
mab105120 <mohd.bourji@gmail.com>
2017-08-30 23:40:56 -0500

    Evaluation page development: - Make form fields required - Change form group location - limit comment text area character and add character count.

--- appraisalconfidence-app/src/main/resources/assets/distro/bundle.js
+++ appraisalconfidence-app/src/main/resources/assets/distro/bundle.js
@@ -138,12 +138,16 @@
     function evaluation_controller($scope, $stateParams, $http) {
         $('#slider1').slider();
         $('#slider1').on('slide', function(slideEvt) {
-            $('#slider1Val').text(slideEvt.value);
+            var val = slideEvt.value;
+            $scope.absConfidence = val;
+            $('#slider1Val').text(val);
         });
 
         $('#slider2').slider();
         $('#slider2').on('slide', function(slideEvt) {
-            $('#slider2Val').text(slideEvt.value);
+            var val = slideEvt.value;
+            $scope.relConfidence = val;
+            $('#slider2Val').text(val);
         });
 
         $scope.selectedEvaluation = '';
@@ -155,10 +159,26 @@
                 $scope.evaluations = response.data;
             },
             function(response) {
+//                alert('Unable to contact the server for reviews');
                 console.log('GET /api/appraisal/evaluations/ ' + response.status);
                 console.log(response);
             }
         );
+
+        // Comment control
+        var comment_max_length = 200;
+        $scope.comment = '';
+        $scope.remainingChars = 200;
+        $scope.calculateRemainingChars = function() {
+            $scope.remainingChars = comment_max_length - $scope.comment.length;
+        }
+
+        // Continue button
+        $scope.continueBtnDisabled = function() {
+            if($scope.comment.length === 0) return true;
+            else
+            return true;
+        };
     };
 
 
