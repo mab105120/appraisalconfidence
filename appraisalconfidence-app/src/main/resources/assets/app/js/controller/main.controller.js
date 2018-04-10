a6d2152de9950c9bce813e4635d33e51791231a8
mab105120 <mohd.bourji@gmail.com>
2018-04-09 23:26:14 -0500

    fix issue where the qualtrics redirect url was being overriden when the application refreshes. Also remove completion code since it will no longer be needed.

--- appraisalconfidence-app/src/main/resources/assets/app/js/controller/main.controller.js
+++ appraisalconfidence-app/src/main/resources/assets/app/js/controller/main.controller.js
@@ -17,14 +17,16 @@
             usSpinnerService.stop('main-spinner');
         }
 
-        var qual_redirect_url = 'https://quninq9ktegh7vsv.co1.qualtrics.com/jfe/preview/SV_abhZZ1YMY2YotCZ?Q_CHL=preview';
-        var params = $location.search();
-        for(var param in params) {
-            if(params.hasOwnProperty(param))
-                qual_redirect_url += '&' + param + '=' + params[param];
+        if(localStorage.getItem('qual_redirect_url') === null) {
+            var qual_redirect_url = 'https://quninq9ktegh7vsv.co1.qualtrics.com/jfe/preview/SV_abhZZ1YMY2YotCZ?Q_CHL=preview';
+            var params = $location.search();
+            for(var param in params) {
+                if(params.hasOwnProperty(param))
+                    qual_redirect_url += '&' + param + '=' + params[param];
+            }
+            localStorage.setItem('qual_redirect_url', qual_redirect_url);
         }
-        console.log(qual_redirect_url);
-        localStorage.setItem('qual_redirect_url', qual_redirect_url);
+
     }
 
     module.exports = main_controller;
